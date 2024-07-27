6cd401ae5208074ff5c3bc9c0e4d6d8a81e5de95
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-07-27 12:22:15 +0530

    106. Using Client Components Efficiently

--- foodies-starting-project/components/main-header/main-header.js
+++ foodies-starting-project/components/main-header/main-header.js
@@ -4,8 +4,10 @@ import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from 'next/image';
 import MainHeaderBackground from './main-header-background';
+import NavLink from './nav-link';
 
 export default function MainHeader() {
+
     return (
         <>
             <MainHeaderBackground />
@@ -16,11 +18,13 @@ export default function MainHeader() {
                 </Link>
                 <nav className={classes.nav}>
                     <ul>
+                       <li>
+                            <NavLink href="/meals" >Browse Meals</NavLink>
+                       </li>
                         <li>
-                            <Link href="/meals">Browse Meals</Link>
-                        </li>
-                        <li>
-                            <Link href="/community">Foodies Community</Link>
+                            <NavLink href="/community">  
+                                Foodies Community
+                            </NavLink>
                         </li>
                     </ul>
                 </nav>
