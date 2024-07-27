e0764690894bbec1ca96b21f097cf18c2b3b352f
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-07-27 20:41:30 +0530

    115. Throwing Not Found Errors For Individual Meals

--- foodies-starting-project/app/not-found.js
+++ foodies-starting-project/app/not-found.js
@@ -2,8 +2,8 @@
 export default function NotFound() {
    return (
       <main className="not-found">
-         <h1>Not Found</h1>
-         <p>Unfortunately, we could not find the requsted page or resource.</p>
+         <h1>Meal Not Found</h1>
+         <p>Unfortunately, we could not find the requsted page or meal data.</p>
       </main>
    );
 }
