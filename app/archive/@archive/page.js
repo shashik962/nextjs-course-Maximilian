b68f687ea55fb26941c1b7e06c705a853cf6cd62
shashi kumar <shashikumar@shashis-MacBook-Air.local>
2024-07-30 23:36:07 +0530

    142. Working with Parallel Routes & Nested Routes

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,4 +1,21 @@
+import Link from "next/link";
+import { getAvailableNewsYears } from "@/lib/news";
+
 
 export default function ArchivePage() {
-   return <h1>Archive Page</h1>;
+   const links = getAvailableNewsYears();
+   console.log(links)
+   return (
+      <header id="archive-header">
+         <nav>
+            <ul>
+               {links.map((link) => (
+                  <li key={link}>
+                     <Link href={`/archive/${link}`}>{link}</Link>
+                  </li>
+               ))}
+            </ul>
+         </nav>
+      </header>
+   );
 }
