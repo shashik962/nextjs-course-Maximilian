582ff3c382b99ee6d39e7b549fbac5ac63d21a83
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-07-30 16:56:03 +0530

    140. Handling "Not Found" Errors & Showing "Not Found" Pages

---
+++ app/not-found.js
@@ -0,0 +1,9 @@
+
+export default function NotFoundPage() {
+    return (
+        <div id="error">
+            <h1>Not Found!</h1>
+            <p>The request resource could not found!</p>
+        </div>
+    );
+}
