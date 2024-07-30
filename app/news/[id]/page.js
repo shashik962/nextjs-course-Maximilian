ca8468a0eabf4232694248f260f9dd0094a23542
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-07-30 11:51:50 +0530

    137. Exercise Solution - Part 1

---
+++ app/news/[id]/page.js
@@ -0,0 +1,10 @@
+
+export default function NewsDetailsPage({ params }) {
+    const newsId = params.id;
+    return (
+        <>
+            <h1>News Details Page</h1>
+            <p>News ID: {newsId}</p>
+        </>
+    )
+}
