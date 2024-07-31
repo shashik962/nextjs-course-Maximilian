ceced778420a5a2b152a872038c2a51ee1e1b87e
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-07-31 16:28:24 +0530

    146. Handling Errors With Error Pages

---
+++ app/archive/@archive/[[...filter]]/error.js
@@ -0,0 +1,10 @@
+"use client"
+
+export default function FilterError({error}) {
+    return (
+        <div>
+            <h2>An error occured!</h2>
+            <p>{error.message}</p>
+        </div>
+    )
+}
