5c9598becc962531d886857adffaa6a1d7849fe7
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-07-31 15:49:35 +0530

    144. Catch-All Fallback Routes & Dealing With Multiple Path Segments

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,6 +1,7 @@
 
 import Link from "next/link";
-import { getAvailableNewsYears } from "@/lib/news";
+import { getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth } from "@/lib/news";
+import NewsList from "@/components/news-list";
 
 
 
@@ -8,21 +9,47 @@ export default function FilterNewsPage({ params }) {
    const filter = params.filter;
    console.log(filter);
 
-   const links = getAvailableNewsYears();
-   // console.log(links);
+   const selectedYear = filter?.[0];
+   const selectedMonth = filter?.[1];
+
+   let news;
+   let links = getAvailableNewsYears();
+
+   if(selectedYear && !selectedMonth) {
+      news = getNewsForYear(selectedYear);
+      links = getAvailableNewsMonths(selectedYear);
+   }
+
+   if(selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth); 
+        links = [];
+   }
+
+   let newsContent = <p>No news found for the selected period.</p>
+
+   if(news && news.length > 0) {
+      newsContent = <NewsList news={news} />
+   }
+
 
    return (
-      <header id="archive-header">
-         <nav>
-            <ul>
-               {links.map((link) => (
-                  <li key={link}>
-                     <Link href={`/archive/${link}`}>{link}</Link>
-                  </li>
-               ))}
-            </ul>
-         </nav>
-      </header>
+      <>
+         <header id="archive-header">
+            <nav>
+               <ul>
+                  {links.map((link) => {
+                     const href = selectedYear ? `/archive/${selectedYear}/${link}` :  `/archive/${link}`;
+                     return (
+                        <li key={link}>
+                           <Link href={href}>{link}</Link>
+                        </li>
+                     )
+                  })}
+               </ul>
+            </nav>
+         </header>
+         {newsContent}
+      </>
    );
 
    // const news = getNewsForYear(newsYear);
