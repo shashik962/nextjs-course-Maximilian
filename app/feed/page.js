393f23fd99dcf4612a62e86f459dbe5984ecdee3
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-08-05 17:05:27 +0530

    203. Configuring Dynamic Page Metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,21 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+
+// export const metadata = {
+//   title: 'Browse all our X Posts',
+//   description: 'Browse all our posts!'
+// }
+
+export async function generateMetadata({searchParams}) {
+    const posts = await getPosts();
+    const numberOfPosts = posts.length;
+    return {
+        title: `Browse all our ${numberOfPosts} posts`,
+        description: 'Browse all our posts!'
+    }
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
