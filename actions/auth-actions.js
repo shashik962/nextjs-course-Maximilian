f51c977513562098494150fe0fac3740b21cca82
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-08-05 19:15:03 +0530

    219. Adding User Login (via a Server Action)

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -39,5 +39,32 @@ export async function signup(prevState, formData) {
     }
     throw error;
   }
-  redirect('/training');
 }
+
+export async function login(prevState, formData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+  
+    const existingUser = getUserByEmail(email);
+  
+    if (!existingUser) {
+      return {
+        errors: {
+          email: 'Could not authenticate user, please check your credentials.',
+        },
+      };
+    }
+  
+    const isValidPassword = verifyPassword(existingUser.password, password);
+  
+    if (!isValidPassword) {
+      return {
+        errors: {
+          password: 'Could not authenticate user, please check your credentials.',
+        },
+      };
+    }
+  
+    await createAuthSession(existingUser.id);
+    redirect('/training');
+  }
