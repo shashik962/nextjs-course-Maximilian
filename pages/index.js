b373e9f8035a61a9d5d03e276b936b885e6dc0bd
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-08-06 16:07:17 +0530

    238. Navigating with the "Link" Component

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ function HomePage() {
       <h1>The Home Page</h1>
       <ul>
           <li>
-              <Link href="/portfolio">Portfolio</Link>
+              <Link href="/portfolio">Portfolio </Link>
           </li>
           <li>
-              <Link href="/clients">Clients</Link>
+              <Link href="/clients">Clients </Link>
           </li>
       </ul>
     </div>
