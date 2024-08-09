f3a33a8f279de8ad9dea8072fbf65fe8656f3445
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2024-08-09 15:35:42 +0530

    356. Adding The "Hero" Component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
-function HomePage()  {}
+import { Fragment } from "react";
+import Hero from "../components/home-page/hero";
+
+function HomePage()  {
+
+    return (
+        <Fragment>
+            <Hero />
+            {/* <FeaturePost /> */}
+        </Fragment>
+    );
+}
 
 export default HomePage;
+
+
+
+
+// 1)  Hero => Present ourselves
+// 2) Featured Posts
